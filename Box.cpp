fb025f56193a5086cab34b28ae121def58396998
Xavier Tan <Xavier-tan@hotmail.sg>
2020-09-03 17:13:07 +0800

    Rewrote box intersection (routine slightly faster)

--- Box.cpp
+++ Box.cpp
@@ -19,16 +19,41 @@ Box::Box(glm::vec3 min, glm::vec3 max, Color c, materialType mat) {
 bool Box::findIntersection(glm::vec3 orig, glm::vec3 dir, 
     float& tNear, int& index, glm::vec2& uv) const {
     // find tminx and tminy and tmaxX and Y 
-    float tminx = (bounds[0].x - orig.x) / dir.x;
-    float tmaxx = (bounds[1].x - orig.x) / dir.x;
-    // in some cases, tmin is greater than tmax depending
-    // on the ray dir, so we swap these values
-    if (tminx > tmaxx) std::swap(tminx, tmaxx);
+    // we want to account for when the direction for any of the 
+    // x y z components is == 0 which also saves on calling swap
+    // function
+    glm::vec3 inverseDir = 1.0f / dir;
 
-    float tminy = (bounds[0].y - orig.y) / dir.y;
-    float tmaxy = (bounds[1].y - orig.y) / dir.y;
-    // same check for which is greater tmin or tmax
-    if (tminy > tmaxy) std::swap(tminy, tmaxy);
+    float tminx, tmaxx;
+    if (inverseDir.x >= .0f) {
+        tminx = (bounds[0].x - orig.x) * inverseDir.x;
+        tmaxx = (bounds[1].x - orig.x)* inverseDir.x;
+    }
+    else {
+        tminx = (bounds[1].x - orig.x) * inverseDir.x;
+        tmaxx = (bounds[0].x - orig.x) * inverseDir.x;
+    }
+
+
+    float tminy, tmaxy;
+    if (inverseDir.y >= .0f) {
+        tminy = (bounds[1].y - orig.y) * inverseDir.y;
+        tmaxy = (bounds[0].y - orig.y) * inverseDir.y;
+    }
+    else {
+        tminy = (bounds[0].y - orig.y) * inverseDir.y;
+        tmaxy = (bounds[1].y - orig.y) * inverseDir.y;
+    }
+    //float tminx = (bounds[0].x - orig.x) / dir.x;
+    //float tmaxx = (bounds[1].x - orig.x) / dir.x;
+    //// in some cases, tmin is greater than tmax depending
+    //// on the ray dir, so we swap these values
+    //if (tminx > tmaxx) std::swap(tminx, tmaxx);
+
+    //float tminy = (bounds[0].y - orig.y) / dir.y;
+    //float tmaxy = (bounds[1].y - orig.y) / dir.y;
+    //// same check for which is greater tmin or tmax
+    //if (tminy > tmaxy) std::swap(tminy, tmaxy);
 
     // check if the ray totally miss x-y planes
     if ((tminx > tmaxy) || (tminy > tmaxx)) 
@@ -41,19 +66,29 @@ bool Box::findIntersection(glm::vec3 orig, glm::vec3 dir,
 
     // after finding max and min re: x-y plane
     // compare tmin and tmax to z max and min now
-    float tminz = (bounds[0].z - orig.z) / dir.z;
-    float tmaxz = (bounds[1].z - orig.z) / dir.z;
-    // same check as before
-    if (tminz > tmaxz) std::swap(tminz, tmaxz);
+    //float tminz = (bounds[0].z - orig.z) / dir.z;
+    //float tmaxz = (bounds[1].z - orig.z) / dir.z;
+    //// same check as before
+    //if (tmin > tmax) std::swap(tmin, tmax);
+
+    float tminz, tmaxz;
+    if (inverseDir.z >= .0f) {
+        tminz = (bounds[1].z - orig.z) * inverseDir.z;
+        tmaxz = (bounds[0].z - orig.z) * inverseDir.z;
+    }
+    else {
+        tminz = (bounds[0].z - orig.z) * inverseDir.z;
+        tmaxz = (bounds[1].z - orig.z) * inverseDir.z;
+    }
+    //if (tminz > tmaxz) std::swap(tminz, tmaxz);
 
     // Check if intersection goes misses z planes
-    if ( (tmin > tmaxz) || (tmax < tminz)) 
+    if ((tmin > tmaxz) || (tmax < tminz)) 
         return false;
 
     // update tmin and tmax values:
     tmin = tmin < tminz ? tminz : tmin;
     tmax = tmax > tmaxz ? tmaxz : tmax;
-    //if (tmin > tmax) std::swap(tmin, tmax);
 
     // the intersection point is the ray's entry pt
     if (tmin > 0.0f) {
@@ -65,12 +100,7 @@ bool Box::findIntersection(glm::vec3 orig, glm::vec3 dir,
         // t value will be negative, which means no hit
         return false;
     }
-    float hitPtZ = orig.z + dir.z * tNear;
-    if ((hitPtZ <= bounds[0].z + .001f) && (hitPtZ >= bounds[0].z - .001f)) {
-        /*N = glm::vec3(0.0f, 0.0f, 1.0f);
-        return;*/
-        int in = 0;
-    }
+
     return true;
 }
 // logic is found here: 
